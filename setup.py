28904e2f7851f6dbb5c2a89c46974d137c60e6f2
Andrew Grigorev <andrew@ei-grad.ru>
2015-12-17 14:43:22 +0300

    Base python package structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from datablimp import __version__ as version
+from datablimp import __doc__ as long_description
+
+
+setup(
+    name='datablimp',
+    version=version,
+
+    author='Andrew Grigorev',
+    author_email='andrew@ei-grad.ru',
+
+    description='ETL framework with bolder approach',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers'
+    ],
+
+    install_requires=[
+
+    ],
+
+    test_suite='datablimp'
+)
