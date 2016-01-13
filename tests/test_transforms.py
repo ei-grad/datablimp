e1da035600979acaacce81bc7cc715dd62a790c9
Andrew Grigorev <andrew@ei-grad.ru>
2016-01-13 12:44:55 +0300

    remove unneeded dateutil

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,5 +1,4 @@
 from datablimp import T
-import dateutil.parser
 
 
 def test_parsetimestamp():
@@ -7,6 +6,4 @@ def test_parsetimestamp():
         'time': 1451463349.251792
     }
     T.ParseTimestamp().transform(doc)
-    assert doc == {
-        'time': dateutil.parser.parse('2015-12-30T11:15:49.251792+00:00')
-    }
+    assert doc['time'].isoformat() == '2015-12-30T11:15:49.251792+00:00'
