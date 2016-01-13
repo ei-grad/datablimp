904c5163ccf397af7587beb69f44180a1b1da204
Andrew Grigorev <andrew@ei-grad.ru>
2016-01-13 12:53:52 +0300

    Fix floating point error in tests

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -3,7 +3,7 @@ from datablimp import T
 
 def test_parsetimestamp():
     doc = {
-        'time': 1451463349.251792
+        'time': 1451463349.25179
     }
     T.ParseTimestamp().transform(doc)
-    assert doc['time'].isoformat() == '2015-12-30T08:15:49.251792+00:00'
+    assert doc['time'].isoformat() == '2015-12-30T08:15:49.25179+00:00'
