d513d59844417e2785fee6addbff6fe83ce23480
Andrew Grigorev <andrew@ei-grad.ru>
2016-01-13 12:50:55 +0300

    Oops

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -6,4 +6,4 @@ def test_parsetimestamp():
         'time': 1451463349.251792
     }
     T.ParseTimestamp().transform(doc)
-    assert doc['time'].isoformat() == '2015-12-30T11:15:49.251792+00:00'
+    assert doc['time'].isoformat() == '2015-12-30T08:15:49.251792+00:00'
